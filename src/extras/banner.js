ae997f6018cab6e360d2e915751d37c815139aea
Jonas <jonas.dongowski@gmail.com>
2022-05-11 14:39:34 +0200

    wip: show json env vars

--- src/extras/banner.js
+++ src/extras/banner.js
@@ -155,7 +155,7 @@ module.exports.printBanner = async () => {
     };
     const boxLines = [
         'All ready! Please access:',
-        chalk.inverse(' ' + process.env + ' '),
+        chalk.inverse(' ' + JSON.stringify(process.env) + ' '),
         ...addrs.map((addr) => chalk.inverse(` http://${addr}:${GlobalData.txAdminPort}/ `)),
         ...adminPinLines,
     ];
